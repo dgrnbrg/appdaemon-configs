7ddcd1843fd34d7dfd404228bfdd44167445d6a7
David Greenberg <dsg123456789@gmail.com>
2022-10-18 09:25:01 -0400

    implement compromise for implementation, task support for goals, clean up dead code

--- temperature.py
+++ temperature.py
@@ -98,6 +98,8 @@ class ClimateGoal(hass.Hass):
         self.preferences = self.args['preferences']
         self.people_trackers = self.args["people_trackers"]
         self.presence_ent = self.args["presence_ent"]
+        self.tasks = self.args.get('tasks',[])
+        self.room = self.args['room']
         runtime = datetime.time(0, 0, 0)
         self.run_in(self.apply_climate_goal, 0)
         self.run_hourly(self.apply_climate_goal, runtime)
@@ -128,12 +130,18 @@ class ClimateGoal(hass.Hass):
             if presence == 'on':
                 target_state = 'agreeable'
                 # TODO incorporatee room-level tracking info here for preference impl
-        # TODO incorporate task overrides here
-        # TODO compute the actual target temperature goal (maybe it's actually a high/low range? or maybe it's a state w/ attributes for preference details)
-        # TODO publish goal for the room
-        self.log(f"setting climate goal for {self.args['room']} to {target_state}")
-        goal_ent = self.get_entity(f"sensor.climate_goal_{self.args['room']}")
         attrs = self.preferences[target_state].copy()
+        self.log(f"setting climate goal for {self.room} to {target_state}, initial attrs {attrs}")
+        for task in self.tasks:
+            if self.get_state(f'input_boolean.{task}') == 'on':
+                task_cfg = self.tasks[task]
+                if 'nudge' in task_cfg: # TODO nothing tests this, yet
+                    attrs['high'] += task_cfg['nudge']
+                    attrs['low'] += task_cfg['nudge']
+                else:
+                    attrs = task_cfg.copy()
+                self.log(f"applying task modifier {task} (cfg={task_cfg}) => new attrs = {attrs}")
+        goal_ent = self.get_entity(f"sensor.climate_goal_{self.room}")
         attrs['temp_sensor'] = self.args['temp_ent']
         goal_ent.set_state(state=target_state, attributes=attrs)
 
@@ -142,48 +150,57 @@ class ClimateImplementor(hass.Hass):
         self.climate_ent = self.args['climate_entity']
         self.weather_ent = self.args['weather_entity']
         self.rooms = self.args['rooms']
-        # New algorithm
-        # We have a single (optional?) climate ents, an optional heating switch, an optional cooling switch, a window/external ventilation switch, a list of goals (ie rooms), outside temp/weather
-        # We'll take the goals, use their adjustments to find a climate ent temp range that satisfies all the goals or the mean with a warning if unsatisfiable
-        # then, we'll consider solutions in a specified priority order
-        # - heating/cooling switches are TODO, since idk if I have any
-        # - window switches should be disabled if the outdoor temp or weather is outside of comfort bounds
-        # - The climate ent should be set to a temp in the valid range closest to the room's current temp. climate settings should be disabled when the outside temp is too extreme.or weather is outside of comfort bounds
-        # - The climate ent should be set to the "cheapest" version 
         self.run_in(self.calculate, 0)
+        runtime = datetime.time(0, 0, 0)
+        self.run_hourly(self.calculate, runtime)
 
     def calculate(self, kwargs):
         thermostat_ent_parts = self.climate_ent.split('.')
         room_goals = {}
         room_calibration = {}
+        has_selfish = False
         for room in self.rooms:
             goal = self.get_state(f"sensor.climate_goal_{room}", attribute='all')
             remote_temp_ent_parts = goal['attributes']['temp_sensor'].split('.')
             calibration = self.get_state(f"sensor.offset_calibrated_{thermostat_ent_parts[1]}_{remote_temp_ent_parts[1]}", attribute='all')
+            if goal['attributes'].get('selfish', False):
+                has_selfish = True
             room_goals[room] = goal
             room_calibration[room] = calibration
         # First decide if we're heating, cooling, or failing (TODO alert somehow)
+        outside_temp = self.get_state(self.weather_ent, attribute='temperature')
         goal_mode = None
         for room in self.rooms:
             goal = room_goals[room]
+            if has_selfish and not goal['attributes'].get('selfish', False):
+                # if a room is being selfish, skip other rooms
+                continue
             calibration = room_calibration[room]
             cur_temp = float(self.get_state(goal['attributes']['temp_sensor']))
-            # TODO incorporate outside temp to block certain goal modes
             if cur_temp <= goal['attributes']['low']:
                 if goal_mode is None:
-                    goal_mode = 'heating'
+                    if outside_temp <= self.args['max_temp_for_heat']:
+                        goal_mode = 'heating'
+                    else:
+                        self.error(f"Want to heat, but it's too warm outside ({outside_temp})")
                 else:
                     self.error(f'goal mode is already {goal_mode} but we want to heat for {self.climate_ent}')
             if cur_temp >= goal['attributes']['high']:
                 if goal_mode is None:
-                    goal_mode = 'cooling'
+                    if outside_temp >= self.args['min_temp_for_ac']:
+                        goal_mode = 'cooling'
+                    else:
+                        self.error(f"Want to cool, but it's too cold outside ({outside_temp})")
                 else:
                     self.error(f'goal mode is already {goal_mode} but we want to cool for {self.climate_ent}')
             self.log(f"after {room} goal mode is {goal_mode}")
 
         if goal_mode is None:
             self.log("Temperature is within comfort range, so not changing thermostat")
+            #self.call_service('climate/set_hvac_mode', entity_id = self.climate_ent, hvac_mode = 'fan_only')
             return
+        
+        # TODO this is where we could try to open a window
 
         # Next find a low-high spread in the climate ent's temp domain
         if goal_mode == 'heating':
@@ -207,8 +224,34 @@ class ClimateImplementor(hass.Hass):
                 target_temp = max(target_temp, room_target_temp)
             elif goal_mode == 'cooling':
                 target_temp = min(target_temp, room_target_temp)
+            self.log(f'after {room} with goal {goal_mode} target climate temp is {target_temp} (room wants {room_target_temp}')
         # next, we must do a pass to see whether this target temp would put any rooms out of spec
         # TODO in this case, we must warn & possibly compromise so that the offset is similar for each room?
+        # find the room that would be most out of bounds for this solution, and compromise with it
+        most_out_of_bounds = None
+        most_out_of_bounds_room = None
+        for room in self.rooms:
+            goal = room_goals[room]
+            calibration = room_calibration[room]
+            offset = calibration['attributes'][f'{goal_mode}_offset']
+            room_expected_temp = target_temp + offset
+            if goal_mode == 'heating' and room_expected_temp > goal['attributes']['high']:
+                self.log(f'{room} would be too hot given the current plan to set at {target_temp}')
+                if most_out_of_bounds is None or most_out_of_bounds < room_expected_temp:
+                    most_out_of_bounds = goal['attributes']['high'] - offset
+                    most_out_of_bounds_room = room
+                    self.log(f'{room} would be most out of bounds, which is {most_out_of_bounds}')
+            elif goal_mode == 'cooling' and room_expected_temp < goal['attributes']['low']:
+                self.log(f'{room} would be too cold given the current plan to set at {target_temp}')
+                if most_out_of_bounds is None or most_out_of_bounds > room_expected_temp:
+                    most_out_of_bounds = goal['attributes']['low'] - offset
+                    most_out_of_bounds_room = room
+                    self.log(f'{room} would be most out of bounds, which is {most_out_of_bounds}')
+        # compromise
+        if most_out_of_bounds is not None:
+            compromise = (most_out_of_bounds + target_temp) / 2.0
+            self.log(f"target would have been {target_temp}, but because of {most_out_of_bounds_room} requiring a minimum setting of {most_out_of_bounds}, we'll compromise to {compromise}")
+            target_temp = compromise
         # set the thermostat
         self.log(f"calculated climate impl: {goal_mode} to {target_temp}")
         if goal_mode == 'heating':
@@ -217,53 +260,3 @@ class ClimateImplementor(hass.Hass):
             target_temp = math.floor(target_temp)
         self.log(f"rounded climate impl to {target_temp}")
         #self.call_service('climate/set_temperature', entity_id = self.climate_ent, temperature = target_temp, hvac_mode = goal_mode[:4])
-
-
-    def calculate_target_temp(self):
-        climate_goal = self.get_state(self.climate_goal)
-        current_temp = self.get_state(self.temperature_ent) # TODO this should be task-linked
-        outside_temp = self.get_state(self.outside_temp)
-
-        if climate_goal == 'safe':
-            t_high = 80 #self.get_state('input_number.temp_safe_high')
-            t_low = 60 #self.get_state('input_number.temp_safe_low')
-        if climate_goal == 'tolerable':
-            t_high = 74
-            t_low = 67
-        if climate_goal == 'agreeable':
-            t_high = 72
-            t_low = 69
-        # todo add logic to incorporate preferences here
-
-        mode = 'fan'
-        target_temp = None
-        if current_temp > t_high:
-            if outside_temp > t_high: # make 73 the min outdoors temp for ac
-                # A/C mode
-                mode = 'cool'
-                target_temp = t_high
-            else:
-                pass # notify to open a window
-        elif current_temp < t_low:
-            if outside_temp < t_low: # make 63 the max outdoors temp for heat
-                mode = 'heat'
-                target_temp = t_low
-            else:
-                pass # notify to open a window
-        else:
-            pass # we're within comfort settings
-
-        if mode != 'fan':
-            # apply calibration
-            climate_ent_parts = self.climate_ent.split('.')
-            temp_ent_parts = self.temperature_ent.split('.')
-            cal = self.get_state(f'sensor.offset_calibrated_{climate_ent_parts[1]}_{temp_ent_parts[1]}')
-            if mode == 'heat':
-                offset = cal['heating_offset']
-            else:
-                offset = cal['cooling_offset']
-            offset = np.clip(offset, -4, 4)
-            target_temp += offset
-            self.call_service('climate/set_temperature', entity_id = self.climate_ent, temperature = target_temp, hvac_mode = mode)
-        else:
-            self.call_service('climate/set_hvac_mode', entity_id = self.climate_ent, hvac_mode = 'fan_only')
