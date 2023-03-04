72378edd7bbe3b8cda1c395d58db7e097eb9de5b
David Greenberg <dsg123456789@gmail.com>
2023-03-04 09:37:31 -0500

    improved lirr fetcher data

--- lirr_fetcher.py
+++ lirr_fetcher.py
@@ -5,7 +5,7 @@ import time
 import requests
 
 
-def fetch_data(from_sta, to_sta):
+def fetch_data(from_sta, to_sta, label):
     def parse_trip(trip):
         duration_min = (trip['trip_end'] - trip['trip_start']) // 60
         legs = trip['legs']
@@ -16,15 +16,11 @@ def fetch_data(from_sta, to_sta):
         sched_time_fmt = sched_time.strftime('%-I:%M %p')
         status = stop['stop_status']
         track = stop['t2s_track']
-        return {'time': sched_time_fmt, 'status': status, 'track': track, 'num_legs': len(legs), 'shuttle': legs[0]['is_shuttle'], 'duration': duration_min}
+        return {'time': sched_time_fmt, 'status': status, 'track': track, 'num_legs': len(legs), 'shuttle': legs[0]['is_shuttle'], 'duration': duration_min, 'numeric_time': stop['sched_time'], 'label': label}
     u = f'https://backend-unified.mylirr.org/plan?from={from_sta}&to={to_sta}&fares=ALL&time={round(time.time() * 1000)}&arrive_by=false'
     r = requests.get(u, headers={'accept': 'application/json', 'accept-version': '3.0', 'dnt': '1'})
     parsed_trips = [parse_trip(t) for t in r.json()['trips']]
     parsed_trips = [t for t in parsed_trips if t['num_legs'] == 1]
-    # drop the departed trips except the most recent
-    num_departed = len([t for t in parsed_trips if t['status'] == 'DEPARTED'])
-    for i in range(num_departed-1):
-        parsed_trips.pop(0)
     return parsed_trips
 
 class LirrFetcher(hass.Hass):
@@ -44,10 +40,20 @@ class LirrFetcher(hass.Hass):
                     'track': trip['track'],
                     'stop_status': trip['status'].replace('_', ' ').capitalize(),
                     'duration': f"{trip['duration']} min",
-                    'friendly_name': f'{self.ordinals[i]} Train',
+                    'friendly_name': trip['label'],#f'{self.ordinals[i]} Train ({trip["label"]})',
                     'icon': 'mdi:train',
                 })
-        to_penn = fetch_data(from_sta='PWS', to_sta='NYK')[:6]
-        publish_entities('lirr_penn', to_penn)
-        to_port = fetch_data(to_sta='PWS', from_sta='NYK')[:6]
-        publish_entities('lirr_pw', to_port)
+        def merge_routes(x, y):
+            routes = x + y
+            routes.sort(key=lambda x: x['numeric_time'])
+            # drop the departed trips except the most recent
+            num_departed = len([t for t in routes if t['status'] == 'DEPARTED'])
+            for i in range(num_departed-1):
+                routes.pop(0)
+            return routes[:6]
+        to_penn = fetch_data(from_sta='PWS', to_sta='NYK', label='Penn Station')
+        to_gc = fetch_data(from_sta='PWS', to_sta='_GC', label='Grand Central')
+        publish_entities('lirr_penn', merge_routes(to_penn, to_gc))
+        from_penn = fetch_data(from_sta='NYK', to_sta='PWS', label='Penn Station')
+        from_gc = fetch_data(from_sta='_GC', to_sta='PWS', label='Grand Central')
+        publish_entities('lirr_pw', merge_routes(from_penn, from_gc))
