06a881def2eb034a6a170c79286af11465f35e63
David Greenberg <dsg123456789@gmail.com>
2023-03-07 09:14:02 -0500

    Harden LIRR fetcher to handle newly observed API behavior

--- lirr_fetcher.py
+++ lirr_fetcher.py
@@ -1,4 +1,5 @@
 import hassapi as hass
+import traceback
 import adbase as ad
 import datetime
 import time
@@ -11,15 +12,29 @@ def fetch_data(from_sta, to_sta, label):
         legs = trip['legs']
         train = legs[0]['train']
         details = train['details']
-        stop = details['stops'][0]
-        sched_time = datetime.datetime.fromtimestamp(stop['sched_time'])
-        sched_time_fmt = sched_time.strftime('%-I:%M %p')
-        status = stop['stop_status']
-        track = stop['t2s_track']
+        try:
+            stop = details['stops'][0]
+            sched_time = datetime.datetime.fromtimestamp(stop['sched_time'])
+            sched_time_fmt = sched_time.strftime('%-I:%M %p')
+        except:
+            sched_time = '??? AM/PM'
+        try:
+            status = stop['stop_status']
+        except:
+            status = 'unknown'
+        try:
+            track = stop['t2s_track']
+        except:
+            track = 'Track unknown'
         return {'time': sched_time_fmt, 'status': status, 'track': track, 'num_legs': len(legs), 'shuttle': legs[0]['is_shuttle'], 'duration': duration_min, 'numeric_time': stop['sched_time'], 'label': label}
-    u = f'https://backend-unified.mylirr.org/plan?from={from_sta}&to={to_sta}&fares=ALL&time={round(time.time() * 1000)}&arrive_by=false'
-    r = requests.get(u, headers={'accept': 'application/json', 'accept-version': '3.0', 'dnt': '1'})
-    parsed_trips = [parse_trip(t) for t in r.json()['trips']]
+    try:
+        u = f'https://backend-unified.mylirr.org/plan?from={from_sta}&to={to_sta}&fares=ALL&time={round(time.time() * 1000)}&arrive_by=false'
+        r = requests.get(u, headers={'accept': 'application/json', 'accept-version': '3.0', 'dnt': '1'})
+        parsed_trips = [parse_trip(t) for t in r.json()['trips']]
+    except:
+        for t in parsed_trips:
+            t['label'] = '(Error) ' + t['label']
+        traceback.print_exc()
     parsed_trips = [t for t in parsed_trips if t['num_legs'] == 1]
     return parsed_trips
 
@@ -49,6 +64,8 @@ class LirrFetcher(hass.Hass):
             # drop the departed trips except the most recent
             num_departed = len([t for t in routes if t['status'] == 'DEPARTED'])
             for i in range(num_departed-1):
+                routes.pop(0)
+            while routes[0]['numeric_time'] + 60*int(self.args.get('max_lookback_mins', 45)) < time.time():
                 routes.pop(0)
             return routes[:6]
         to_penn = fetch_data(from_sta='PWS', to_sta='NYK', label='Penn Station')
